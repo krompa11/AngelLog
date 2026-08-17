6d454a9777a7a85fe32f3d389d9fd148b14ca9ff
krompa11 <krompa11@gmail.com>
2026-08-18 00:13:27 +0200

    Load AngelLog v6 user moderation controls

--- v6-globals.js
+++ v6-globals.js
@@ -2,4 +2,9 @@
   try{Object.defineProperty(window,'aaUser',{configurable:true,get:()=>aaUser,set:v=>{aaUser=v}})}catch{}
   try{Object.defineProperty(window,'aaCurrentWater',{configurable:true,get:()=>aaCurrentWater,set:v=>{aaCurrentWater=v}})}catch{}
   try{Object.defineProperty(window,'aaMap',{configurable:true,get:()=>aaMap})}catch{}
+  setTimeout(()=>{
+    if(!document.querySelector('script[src="/v6-user-moderation.js"]')){
+      const s=document.createElement('script');s.src='/v6-user-moderation.js';document.body.appendChild(s)
+    }
+  },450)
 })();
